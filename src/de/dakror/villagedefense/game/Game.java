19a4273c3ca4a1a272869d2e6872f55a288af348
Dakror <mail@dakror.de>
2013-11-22 12:50:11 +0100

    Reordered buildableStructs

--- src/de/dakror/villagedefense/game/Game.java
+++ src/de/dakror/villagedefense/game/Game.java
@@ -66,7 +66,7 @@ public class Game extends EventListener
 	public static Game currentGame;
 	public static JFrame w;
 	public static World world;
-	public static Struct[] buildableStructs = { new Way(0, 0), new House(0, 0), new Farm(0, 0), new Windmill(0, 0), new Bakery(0, 0), new Mine(0, 0), new Sawmill(0, 0), new CoalFactory(0, 0), new Smeltery(0, 0), new Marketplace(0, 0), new School(0, 0), new Warehouse(0, 0), new ArrowTower(0, 0), new Catapult(0, 0), new Barricade(0, 0) };
+	public static Struct[] buildableStructs = { new Way(0, 0), new House(0, 0), new Farm(0, 0), new Windmill(0, 0), new Bakery(0, 0), new Mine(0, 0), new Sawmill(0, 0), new CoalFactory(0, 0), new Smeltery(0, 0), new Marketplace(0, 0), new School(0, 0), new Warehouse(0, 0), new Barricade(0, 0), new ArrowTower(0, 0), new Catapult(0, 0) };
 	
 	static HashMap<String, BufferedImage> imageCache = new HashMap<>();
 	
